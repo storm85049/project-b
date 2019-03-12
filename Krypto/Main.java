20b47a4e61f732af203fd00d785f6444c58ff1ed
Hencom <simon9629@gmail.com>
2019-03-12 19:28:47 +0100

    Bericht angefangen.

--- Krypto/Main.java
+++ Krypto/Main.java
@@ -5,20 +5,26 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
+        ISymmetricEncryption rc4B = new RC4(), rc4A = new RC4("10100100");
 
-        String [][] key = {
-                {"5","24", "1"},
-                {"13","3", "10"},
-                {"20","17", "24"},
-        };
-        try {
-            ISymmetricEncryption hillA = new HillCipher(key, "ASCII");
-            System.out.println(hillA.encrypt("Hello this is a test"));
-            System.out.println(hillA.decrypt(hillA.encrypt("Hello this is a test")));
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        // A --------> Sends Key to B
+        rc4B.setSelectedKeys(rc4A.getKeyMap());
+        // A --------> Sends Encrypted message to B
+        String encryptedMA = rc4A.encrypt("Hello world, It´s a me, MARIO!");
+        System.out.println("Encrypted message A ----> B " + encryptedMA);
+        // B --------> Decrypts Message
+        System.out.println("Decrypted message B: " + rc4B.decrypt(encryptedMA));
+
+        // Answer from B with new Random key
+        rc4B.generateRandomKey();
+
+        // B --------> Sends Key to B
+        rc4A.setSelectedKeys(rc4B.getKeyMap());
+        // A --------> Sends Encrypted message to B
+        String encryptedMB = rc4B.encrypt("I´m answering with a RANDOM KEY // 154786==!`())()()()??");
+        System.out.println("Encrypted message B ----> A " + encryptedMB);
+        // A --------> Decrypts Message
+        System.out.println("Decrypted message B: " + rc4A.decrypt(encryptedMB));
 
     }
 }
--- Krypto/RC4.java
+++ Krypto/RC4.java
@@ -29,7 +29,7 @@ import java.util.Random;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // CORRECT RANDOM KEY GENERATION!!!
-public class RC4 implements ISymmetricEncryption{
+public class RC4 implements ISymmetricEncryption {
     private static final short BYTE_AMOUNT = 8;
     private static final short SIZE = 256;
 
@@ -38,19 +38,20 @@ public class RC4 implements ISymmetricEncryption{
     private Map<String, String> keyMap = new HashMap<>();
 
     //Constructor
-    public RC4(String key){
+    public RC4(String key) {
         keyMap.put("key", key);
         keyMap.put("is_key_random", "false");
         setSelectedKeys(keyMap);
     }
 
     // Empty Constructor for later addition of keys/ Random generation of key
-    public RC4(){}
+    public RC4() {
+    }
 
     @Override
     public String encrypt(String m) {
         char[] encryptedCharArray = new char[m.length()];
-        for(int i = 0; i < m.length(); i ++){
+        for (int i = 0; i < m.length(); i++) {
             short encryptionTempKey = pseudoRandomNumberFromSeed(s);
             int intCharacter = m.toCharArray()[i];
             intCharacter = (intCharacter ^ encryptionTempKey);
@@ -66,19 +67,22 @@ public class RC4 implements ISymmetricEncryption{
 
     @Override
     public void setSelectedKeys(Map<String, String> keyMap) {
-        for(short i = 0; i < s.length; i++){s[i] = i;} // Generate sequential s
-        if(keyMap.get("is_key_random").equalsIgnoreCase("false")){
+        for (short i = 0; i < s.length; i++) {
+            s[i] = i;
+        } // Generate sequential s
+        if (keyMap.get("is_key_random").equalsIgnoreCase("false")) {
             k = calculateKArray(keyMap.get("key"));
+        } else {
+            keyMap.put("is_key_random", "false");
         }
-        else{
-        keyMap.put("is_key_random", "false");}
         this.keyMap = keyMap;
         // Scramble
         scrambleSeed(s);
     }
 
     @Override
-    public void setAlphabetMode(String mode) {}
+    public void setAlphabetMode(String mode) {
+    }
 
     @Override
     public Map<String, String> getKeyMap() {
@@ -94,10 +98,12 @@ public class RC4 implements ISymmetricEncryption{
     public void generateRandomKey() {
         keyMap.put("is_key_random", "true");
         Random rnd = new Random();
-        for(short element : k){
+        for (short element : k) {
             element = (short) rnd.nextInt(2);
         }
-        for(short i = 0; i < s.length; i++){s[i] = i;} // Generate sequencial s
+        for (short i = 0; i < s.length; i++) {
+            s[i] = i;
+        } // Generate sequencial s
         // Scramble
         scrambleSeed(s);
     }
@@ -107,25 +113,25 @@ public class RC4 implements ISymmetricEncryption{
         return this.keyMap.get("key");
     }
 
-    private short[] calculateKArray(String key){
+    private short[] calculateKArray(String key) {
         // Transforms a String into a short binary array.
         short[] k = new short[SIZE];
         char[] tempByteArray = key.toCharArray();
         short counter = 0;
         outerLoop:
-        for(short i = 0; i < k.length; i+=BYTE_AMOUNT){
+        for (short i = 0; i < k.length; i += BYTE_AMOUNT) {
             if (counter == tempByteArray.length) {
                 counter = 0;
             }
             middleLoop:
-            for(short x = 0; x < tempByteArray.length; x++){
-                short temp = (short)tempByteArray[x];
+            for (short x = 0; x < tempByteArray.length; x++) {
+                short temp = (short) tempByteArray[x];
                 innerLoop:
-                for(short j = 0; j < BYTE_AMOUNT; j++){
-                    if(j+i+(x*BYTE_AMOUNT) >= SIZE){
+                for (short j = 0; j < BYTE_AMOUNT; j++) {
+                    if (j + i + (x * BYTE_AMOUNT) >= SIZE) {
                         break outerLoop;
                     }
-                    k[i+j+(x*BYTE_AMOUNT)] = (short)(((temp & (1<<(BYTE_AMOUNT - 1 -j))) != 0) ? 1 : 0);
+                    k[i + j + (x * BYTE_AMOUNT)] = (short) (((temp & (1 << (BYTE_AMOUNT - 1 - j))) != 0) ? 1 : 0);
                 }
             }
             counter++;
@@ -133,10 +139,10 @@ public class RC4 implements ISymmetricEncryption{
         return k;
     }
 
-    private short[] scrambleSeed(short[] s){
+    private short[] scrambleSeed(short[] s) {
         // Scrambles Seed Array (s) with the Key array (k) 256 times
         int j = 0;
-        for(int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {
             j = (j + s[i] + k[i]) % SIZE;
             // Swap
             short temp = s[i];
@@ -146,7 +152,7 @@ public class RC4 implements ISymmetricEncryption{
         return s;
     }
 
-    private short pseudoRandomNumberFromSeed(short[] s){
+    private short pseudoRandomNumberFromSeed(short[] s) {
         // Generates a PRNM for the Encryption with RC4
         int i = 0, j = 0;
         i = (i + 1) % SIZE;
